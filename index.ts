35b1d9640a1cd9eebef7cc61e9a126e762b3d983
S MUZAMMIL JAVED <s.muzammiljaved@gmail.com>
2024-03-25 23:22:56 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+const genrateNumber=Math.floor(Math.random()*6 +1);
+const answer=await inquirer.prompt([
+    {
+   message:("Guess Number between 1-6"),
+   type:"number",
+   name:'input'
+}
+]);
+console.log("computer guess:"+ genrateNumber);
+if(genrateNumber===answer.input){
+    console.log("You Win");
+}else{
+    console.log("You Lost");
+}
